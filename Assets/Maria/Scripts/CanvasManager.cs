723f517f6d6dd6faf87ff06c3e31571620c89ba0
Maria Claverias <143734728+mariaclaverias@users.noreply.github.com>
2023-12-05 15:45:53 -0500

    agregado de scripts

---
+++ Assets/Maria/Scripts/CanvasManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasManager : MonoBehaviour
+{
+    public void OnClick_SendData()
+    {
+        Debug.Log("Datos Enviados");
+    }
+}
